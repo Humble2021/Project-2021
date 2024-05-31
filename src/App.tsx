3b23a25db4184034f24f1809b2a328dd7cfa8c64
Humble2021 <guno.efren2021@gmail.com>
2024-06-01 03:16:45 +0800

    Updated components in src (App.tsx, main.tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import {Routes, Route} from "react-router-dom";
+import Login from "./pages/Login";
 
 function App() {
-  return <h1>hi</h1>;
+  return (
+    <>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
+    </>
+  );
 }
 
 export default App;
